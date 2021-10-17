8b697a413cf341c628ef34f869ff971dedc65c7a
reza babakhani <babakhani.reza@gmail.com>
2021-10-17 23:53:48 +0330

    fix vutify mobile breakpoint

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,6 +6,9 @@ Vue.use(Vuetify)
 export default (APP_CONFIG) => {
   return new Vuetify({
     rtl: APP_CONFIG.rtl,
+    breakpoint: {
+      mobileBreakpoint: 'xs' // This is equivalent to a value of 960
+    },
     theme: {
       dark: APP_CONFIG.darkTheme,
       themes: APP_CONFIG.themes,
