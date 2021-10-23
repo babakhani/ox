b03fe0c7fd9bbd605bed1d93b6c6270aec4bf6d1
reza babakhani <babakhani.reza@gmail.com>
2021-10-24 01:52:21 +0330

    time filter has been added

--- src/filters/index.js
+++ src/filters/index.js
@@ -3,6 +3,14 @@ import Vue from 'vue'
 import PersianDate from 'persian-date'
 
 export default (APP_CONFIG) => {
+  Vue.filter('time', (date, hasTime) => {
+    let result = null
+    if (date) {
+      const newDate = new Date(date)
+      result = new PersianDate(newDate).format('HH:mm')
+    }
+    return result
+  })
   Vue.filter('shortDate', (date, hasTime) => {
     let result = null
     if (date) {
